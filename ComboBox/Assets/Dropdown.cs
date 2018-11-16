083b03ad27693989b163fcc80570546239c4019c
zhuoIC <nicolimonar@gmail.com>
2018-11-16 13:45:13 +0100

    Hecho proyecto con multiples opciones
    Empezado el juego con control mediante botones

---
+++ ComboBox/Assets/Dropdown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//------------------------------------
+using UnityEngine.UI;
+
+public class Dropdown : MonoBehaviour {
+
+	public Text opcion;
+	public UnityEngine.UI.Dropdown _dropdown;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OpcionSeleccionada(){
+		opcion.text = _dropdown.value.ToString() + " -> " + _dropdown.options[_dropdown.value].text;
+	}
+}
