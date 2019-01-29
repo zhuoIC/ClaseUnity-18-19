0e8d7602fc6e740f7b5918a221f0f18e045c427a
zhuoIC <nicolimonar@gmail.com>
2019-01-29 14:40:30 +0100

    Ejemplo de Prefab y Sonidos

---
+++ Capas/Assets/Scripts/RaySphere.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaySphere : MonoBehaviour {
+	public Text txtInfo;
+	public int mask =1 << 9;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate(){
+		Vector3 origin = transform.position;
+		Vector3 direction = Vector3.forward;
+		float distance = Mathf.Infinity;
+		RaycastHit info;
+		Debug.DrawLine(origin,direction * 20,Color.blue);
+		if(Physics.Raycast(origin, direction, out info,distance,mask)){
+			txtInfo.text = "Objeto detectado: "+info.transform.name;
+		}else{
+			txtInfo.text = string.Empty;
+		}
+	}
+}
---
+++ Capas/Assets/Scripts/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+	public AudioSource audio;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(1.33f,1.33f,1.33f);
+		if(Input.GetKeyDown(KeyCode.Space)){
+			audio.Play();
+			Destroy(gameObject);
+		}
+	}
+}
